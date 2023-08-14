faf51cd88403abf18d90dfab9aa669f3eff396ec
wooni <sjchu91@gmail.com>
2023-08-14 14:23:21 +0900

    [Wooni-9999] calculate v1.

--- libs/application/src/main/java/sample/wooni/place/service/search/PlaceSearchServiceImpl.java
+++ libs/application/src/main/java/sample/wooni/place/service/search/PlaceSearchServiceImpl.java
@@ -1,7 +1,6 @@
 package sample.wooni.place.service.search;
 
 
-import com.google.common.collect.Lists;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,7 +9,10 @@ import sample.wooni.place.service.search.dto.PlaceSearchDto;
 import sample.wooni.place.service.search.dto.PlaceSearchResultDetailDto;
 import sample.wooni.place.service.search.external.ExternalPlaceSearchService;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -36,30 +38,35 @@ public class PlaceSearchServiceImpl implements PlaceSearchService {
                 it -> searchResults.addAll(it.search(request.keyword()))
         );
 
-        Map<String, List<PlaceSearchResultDetailDto>> addressToDetailMap = searchResults.stream().collect(
-                Collectors.groupingBy(PlaceSearchResultDetailDto::roadAddress)
-        );
+        var addressGrouping = searchResults.stream()
+                .collect(Collectors.groupingBy(PlaceSearchResultDetailDto::roadAddress));
 
-        return addressToDetailMap.values().stream()
-                .filter(it -> it.size() > 0)
-                .map(this::buildResponse)
-                .sorted(Comparator.comparing(PlaceResponseDto::hitCount).reversed())
-                .toList();
+        var result = new ArrayList<PlaceResponseDto>();
+        for (PlaceSearchResultDetailDto detail : searchResults) {
+            var keywords = addressGrouping.get(detail.roadAddress());
+
+            if (Objects.nonNull(keywords)) {
+                result.add(buildResponse(keywords));
+                addressGrouping.put(detail.roadAddress(), null);
+            }
+        }
+
+        result.sort(Comparator.comparing(PlaceResponseDto::hitCount).reversed());
+        return result;
     }
 
-    private PlaceResponseDto buildResponse(List<PlaceSearchResultDetailDto> details) {
-        var firstItem = details.get(0);
+    private PlaceResponseDto buildResponse(List<PlaceSearchResultDetailDto> keywords) {
+        var firstItem = keywords.get(0);
 
         return PlaceResponseDto.builder()
                 .keyword(firstItem.keyword())
-                .hitCount(details.size())
                 .type(firstItem.type())
-                .relatedKeyword(details.size() > 1 ? details.subList(1, details.size()).stream().map(this::buildKeyword).toList() : Lists.newArrayList())
+                .hitCount(keywords.size())
+                .relatedKeyword(keywords.subList(1, keywords.size()).stream().map(this::buildKey).toList())
                 .build();
     }
 
-    private String buildKeyword(PlaceSearchResultDetailDto detail) {
+    private String buildKey(PlaceSearchResultDetailDto detail) {
         return String.format("%s (%s)", detail.keyword(), detail.type());
     }
-
 }
