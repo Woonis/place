50f8cb5aa3af72f36edf4491d31ef55e9bed0bf5
wooni <sjchu91@gmail.com>
2023-08-14 14:54:16 +0900

    [Wooni-9999] calculate v1.

--- libs/adapter-http/src/main/java/sample/wooni/place/common/ValueConvertUtils.java
+++ libs/adapter-http/src/main/java/sample/wooni/place/common/ValueConvertUtils.java
@@ -30,11 +30,8 @@ public class ValueConvertUtils {
         double coorY = Double.parseDouble(y);
 
         GeoPoint in_pt = new GeoPoint(coorX, coorY);
-        System.out.println("geo in : xGeo="  + in_pt.getX() + ", yGeo=" + in_pt.getY());
         GeoPoint tm_pt = GeoTrans.convert(GeoTrans.GEO, GeoTrans.TM, in_pt);
-        System.out.println("tm : xTM=" + tm_pt.getX() + ", yTM=" + tm_pt.getY());
         GeoPoint katec_pt = GeoTrans.convert(GeoTrans.TM, GeoTrans.KATEC, tm_pt);
-        System.out.println("katec : xKATEC=" + katec_pt.getX() + ", yKATEC=" + katec_pt.getY());
 
         return new ValueConvertUtils.GeoDetailDto(BigDecimal.valueOf(katec_pt.getX()), BigDecimal.valueOf(katec_pt.getY()));
     }
